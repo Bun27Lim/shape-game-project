3f85d97ff26b2f8f8317cdf064a357c6329f7ba4
Alan Lim <bunchhl@g.clemson.edu>
2021-04-24 14:43:26 -0400

    Added Image Library and a Texture Manager class for easier rendering

---
+++ FinalGame/FinalGame/TextureManager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "GameEngine.h"
+
+class TextureManager {
+
+	public:
+		static SDL_Texture* LoadTexture(const char* file);
+		static void Render(SDL_Texture* texture, SDL_Rect src, SDL_Rect dest);
+
+	private:
+};
