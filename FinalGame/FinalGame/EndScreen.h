0f39a1bf017278c1601bde54243626b3b5826569
jwils23 <jwils23@g.clemson.edu>
2021-04-28 16:22:32 -0400

    added endscreen

---
+++ FinalGame/FinalGame/EndScreen.h
@@ -0,0 +1,19 @@
+#ifndef END_H
+#define END_H
+
+#include "GameObject.h"
+
+class EndScreen : public GameObject {
+
+public:
+
+	void es_init(const char* graphic, SDL_Renderer* ren, int start_x, int start_y, int start_w, int start_h);
+
+
+private:
+
+};
+
+
+
+#endif
